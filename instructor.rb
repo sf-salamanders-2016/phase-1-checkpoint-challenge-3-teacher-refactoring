9d805ea607e99f84d9049d6d1cf2958622185ecd
pongo-pygmaeus <maxwell.mathews@devbootcamp.com>
2016-08-11 10:03:51 -0700

    Some refactoring

--- instructor.rb
+++ instructor.rb
@@ -5,11 +5,21 @@ class Instructor < SchoolMember
 	attr_reader :salary, :target_raise
 
 	def salary=(new_salary)
+		#Should be in a more generic school employee class
 		puts "This better be good!"
 		@salary = new_salary
 	end
 
+	def teach_stuff
+	    response = "Default teaching stuff, blah, blah, blah"
+	end
+
+	def set_performance_rating(rating)
+		response = "Default performance_rating. Should probably be in a more generic school employee class"
+	end
+
 	def receive_raise(raise)
+		#Should be in a more generic school employee class
 		@salary += raise
 	end
 
