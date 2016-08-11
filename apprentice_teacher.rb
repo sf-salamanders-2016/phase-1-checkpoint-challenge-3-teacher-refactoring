ad7cc5b8afb0bf01ab3911b44dfcbb73269ccd62
Connor Ray <email": null>
2016-08-11 10:17:07 -0700

    Adds teach stuff and set performance rating to teacher module

--- apprentice_teacher.rb
+++ apprentice_teacher.rb
@@ -5,30 +5,17 @@ class ApprenticeTeacher < PeopleOfClassroom
   include Teachers
   attr_reader :salary, :target_raise
 
-  def initialize(options={})
-    @age = options.fetch(:age, 0)
-    @name = options.fetch(:name, "")
+  ADJECTIVE = "crazy"
+  ENDING_PHRASE = nil
+  DESCRIPTION = "."
+  RATING = 80
+  RESPONCE = "Oh, well -- thanks to this actionable, specific, and kind feedback, I'll do better next time."
+
+   def initialize(options={})
     @target_raise = 800
     @phase = 3
-  end
-
-  def teach_stuff
-    response = ""
-    response += "Listen, class, this is how everything works. "
-    response += "*drops crazy knowledge bomb* "
-    response += "... You're welcome."
-    response
-  end
-
-  def set_performance_rating(rating)
-    response = ""
-    if rating > 80
-      response = "Yay, I'm a great employee!"
-      receive_raise(@target_raise)
-    else
-      response = "Oh, well -- thanks to this actionable, specific, and kind feedback, I'll do better next time."
-    end
-    response
+    @age = options.fetch(:age, 0)
+    @name = options.fetch(:name, "")
   end
 
   def attend_training_session
--- teacher.rb
+++ teacher.rb
@@ -5,30 +5,17 @@ class Teacher < PeopleOfClassroom
   include Teachers
   attr_reader :salary, :performance_rating, :target_raise
 
+  ADJECTIVE = "flat-out insane"
+  ENDING_PHRASE = " *saunters away*"
+  DESCRIPTION = ", fo SHO!"
+  RATING = 90
+  RESPONCE = "Oh, well -- thanks to this actionable, specific, and kind feedback, I'll do better next time."
+
   def initialize(options={})
-    @age = options.fetch(:age, 0)
-    @name = options.fetch(:name, "")
     @phase = 3
     @target_raise = 1000
+    @age = options.fetch(:age, 0)
+    @name = options.fetch(:name, "")
   end
 
-  def teach_stuff
-    response = ""
-    response += "Listen, class, this is how everything works, fo SHO! "
-    response += "*drops flat-out insane knowledge bomb* "
-    response += "... You're welcome. *saunters away*"
-    response
-  end
-
-  def set_performance_rating(rating)
-    response = ""
-    if rating > 90
-      receive_raise(@target_raise)
-      response = "Yay, I'm a great employee!"
-    else
-      response += "Oh, well -- thanks to this actionable, specific, and kind "
-      response += "feedback, I'll do better next time."
-    end
-    response
-  end
 end
