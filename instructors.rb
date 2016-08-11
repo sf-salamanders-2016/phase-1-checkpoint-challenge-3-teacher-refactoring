e772a11b31f486d8e99ff8d7f1015943951cc724
Youna Yang <ayounayang@gmail.com>
2016-08-11 10:48:00 -0700

    Update instructors.rb

--- instructors.rb
+++ instructors.rb
@@ -16,4 +16,15 @@ class Instructors < Person
     @salary += raise
   end
 
+  def set_performance_rating(rating)
+    response = ""
+    if rating > self.class::RATING
+      response = "Yay, I'm a great employee!"
+      receive_raise(@target_raise)
+    else
+      response = "Oh, well -- thanks to this actionable, specific, and kind feedback, I'll do better next time."
+    end
+    response
+  end
+
 end
